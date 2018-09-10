2a47e8835465a4a1de16d40b80dbd5d125f7c0e7
Nicolas Benito <nicobenito10@gmail.com>
2018-09-10 18:03:07 -0300

    added view and dismiss functionality

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,11 +4,16 @@ import '../style/Post.css';
 class Post extends Component {
     constructor(props) {
         super(props);
-        this.state = { }
+        this.state = {
+            visited: false,
+            removed: false
+         }
         this.selectPost = this.selectPost.bind(this);
+        this.dismissPost = this.dismissPost.bind(this);        
       }
     
       selectPost() {
+        this.setState({visited: true});
         this.props.onSelect({
             title: this.props.title,
             imgUrl:this.props.imgPath,
@@ -16,15 +21,22 @@ class Post extends Component {
         });
       };
 
+      dismissPost(e) {
+        e.stopPropagation();
+        this.setState({removed: true});
+      };
+    
     render() {
         return (
-        <div className="Post" onClick={this.selectPost}>
+        <div className={this.state.removed ? 'hiddenPost' : 'Post'} onClick={this.selectPost}>
             <div>
+                <span className={this.state.visited ? 'visited' : 'notVisited'}>Viewed</span>
                 <h1>{this.props.title}</h1>
                 <img src={this.props.imgPath}></img>
                 <h3>Voted: {this.props.voted}</h3>
                 <h3>{this.props.comments} comments</h3>
-                <span>Added {this.props.created} ago</span>
+                <span>Added {this.props.created} hours ago</span>
+                <button onClick={this.dismissPost}> Dismiss post </button>
             </div>
         </div>
         );
