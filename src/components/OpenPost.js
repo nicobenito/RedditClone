16eb7ae33212ab86631c65470417f709298cb2d1
Nicolas Benito <nicobenito10@gmail.com>
2018-09-10 14:19:39 -0300

    created project base - installed dependencies - added components files

---
+++ src/components/OpenPost.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import '../style/App.css';
+
+class OpenPost extends Component {
+  render() {
+    return (
+      <div className="OpenPost">
+        <div>
+            
+        </div>
+      </div>
+    );
+  }
+}
+
+export default OpenPost;
---
+++ src/components/Post.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Post extends Component {
+  render() {
+    return (
+      <div className="Post">
+        <div>
+            
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Post;
